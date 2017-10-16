4b2f086b6cd9ee958f78902620d16e34e277d07e
Varfog <Varfog@email.ru>
2017-10-16 14:15:46 +0300

    -

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ function wait(){
 		var e = document.createElement('script');
 		e.type = 'text/javascript';
 		e.setAttribute('defer','');
-		e.src = chrome.extension.getURL('gladiatus.js');
+		e.src = chrome.extension.getURL('base.js');
 		var h = document.getElementsByTagName('head')[0];
 		h.insertBefore(e, h.firstChild);
 		var UICSS = document.createElement('link');UICSS.rel="stylesheet";UICSS.type="text/css";UICSS.media="screen";UICSS.href=chrome.extension.getURL("css/gladiatusUI.css");
